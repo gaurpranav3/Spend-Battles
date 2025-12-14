097f251b2bfa10bc4771d9e6f75c2d3a92f794fc
gaurpranav3 <146327419+gaurpranav3@users.noreply.github.com>
2025-12-14 22:04:23 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://hibpdokwcovgbcbalauw.supabase.co";
 const SUPABASE_KEY = "sb_publishable_9Q3Zf6l3TkNqk3TdZ0_rng_YD7iNJtm";
 
-const supabaseClient = supabase.createClient(
+const supabase = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
