25f528a89eb10fc4f8495c119a56ffde7ea56083
gaurpranav3 <146327419+gaurpranav3@users.noreply.github.com>
2025-12-14 21:59:45 +0530

    add auth logic

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,37 @@
+async function signUp() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const status = document.getElementById("authStatus");
 
+  status.innerText = "Creating account...";
+
+  const { error } = await supabaseClient.auth.signUp({
+    email,
+    password
+  });
+
+  if (error) {
+    status.innerText = error.message;
+  } else {
+    status.innerText = "Account created. Check your email 📩";
+  }
+}
+
+async function signIn() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const status = document.getElementById("authStatus");
+
+  status.innerText = "Signing in...";
+
+  const { error } = await supabaseClient.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    status.innerText = error.message;
+  } else {
+    window.location.href = "home.html";
+  }
+}
