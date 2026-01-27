2fd03c02ccf07061619da920dbfb9b9e4916f026
gaurpranav3 <146327419+gaurpranav3@users.noreply.github.com>
2026-01-27 11:37:29 +0530

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,10 @@
+export function todayDate() {
+  return new Date().toISOString().split("T")[0];
+}
+
+export function getMonday(date = new Date()) {
+  const d = new Date(date);
+  const day = d.getDay();
+  const diff = d.getDate() - day + (day === 0 ? -6 : 1);
+  return new Date(d.setDate(diff)).toISOString().split("T")[0];
+}
